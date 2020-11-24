f8e4952928fb790c13265b77bdc2de6e229e4118
embed-Rayn <57181674+embed-Rayn@users.noreply.github.com>
2020-11-24 17:23:10 +0900

    update basic

---
+++ pygame_basic/7_text.py
@@ -0,0 +1,133 @@
+import pygame
+import random
+random.randint(1, 4)
+pygame.init() # 초기화 (반드시 필요)
+
+# 화면 크기 설정
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("Nado game") # 게임 이름
+
+# FPS
+clock = pygame.time.Clock()
+
+# 배경 이미지 불러오기
+background = pygame.image.load("D:\\PycharmProject\\pygame\\pygame_basic\\background.png")
+
+# 캐릭터(스프라이트 불러오기)
+character = pygame.image.load("D:\\PycharmProject\\pygame\\pygame_basic\\character.png")
+character_size = character.get_rect().size # 이미지의 크기를 구해옴
+character_width = character_size[0]     # 캐릭터의 가로크기
+character_height = character_size[1]    # 캐릭터의 세로 크기
+character_x_pos = screen_width / 2  - character_width /2 # 화면 가로의 절반 크기에 해당하는 곳에 위치(가로)
+character_y_pos = screen_height - character_height        # 화면 세로 크기 가장 아래에 해당하는 곳에 위치(세로)
+
+# 이동할 좌표
+to_x = 0
+to_y = 0
+
+# 이동 속도
+game_speed = 1
+
+# 적 enemy 캐릭터
+enemy = [pygame.image.load("D:\\PycharmProject\\pygame\\pygame_basic\\enemy.png") for _ in range(10)]
+len_enemy= len(enemy)
+enemy_size = [enemy[i].get_rect().size for i in range(len_enemy)] # 이미지의 크기를 구해옴
+enemy_width = [enemy_size[i][0] for i in range(len_enemy)]     # 캐릭터의 가로크기
+enemy_height = [enemy_size[i][1] for i in range(len_enemy)]    # 캐릭터의 세로 크기
+enemy_x_pos = [random.randint(0, screen_width) for i in range(len_enemy)] # 화면 가로의 절반 크기에 해당하는 곳에 위치(가로)
+enemy_y_pos = [random.randint(0, screen_height / 2) for i in range(len_enemy)]      # 화면 세로 크기 가장 아래에 해당하는 곳에 위치(세로)
+
+# 폰트 정의
+game_font = pygame.font.Font(None, 40) # 폰트 객체 생성(폰트, 크기)
+
+# 총 시간
+total_time = 10
+# 시간 시작
+start_ticks = pygame.time.get_ticks() # 시작 tick을 받아옴
+
+
+# 이벤트 루프
+running = True # 게임이 진행 중인가?
+cnt = 0
+while running:
+    cnt += 1
+    dt = clock.tick(60) # 게임 화면의 초당 프레임 수를 설정
+
+    print("fps : ", clock.get_fps())
+    for event in pygame.event.get(): # 어떤 이벤트가 발생하였는가
+        if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하였는가?
+            running = False # 게임이 진행중이 아님
+
+        if event.type == pygame.KEYDOWN: # 키가 눌렸는지 확인
+            if event.key == pygame.K_LEFT:
+                to_x -= game_speed
+            elif event.key == pygame.K_RIGHT:
+                to_x += game_speed
+            elif event.key == pygame.K_UP:
+                to_y -= game_speed
+            elif event.key == pygame.K_DOWN:
+                to_y += game_speed
+        if event.type == pygame.KEYUP: # 키가 눌렸는지 확인
+            if event.key == pygame.K_UP or pygame.K_DOWN:
+                to_y = 0
+            if event.key == pygame.K_LEFT or pygame.K_RIGHT:
+                to_x = 0
+    character_y_pos += to_y * dt
+    character_x_pos += to_x * dt
+
+    # 가로 경계값 처리
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width-character_width:
+        character_x_pos = screen_width-character_width
+
+    # 세로 경계값 처리
+    if character_y_pos < 0:
+        character_y_pos = 0
+    elif character_y_pos > screen_height - character_height:
+        character_y_pos = screen_height - character_height
+    #충돌 처리
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+    enemy_rect = []
+    for i in range(len_enemy):
+        enemy_rect.append(enemy[i].get_rect())
+        enemy_rect[i].left = enemy_x_pos[i]
+        enemy_rect[i].top = enemy_y_pos[i]
+
+    # 출돌 체크
+    for i in range(len_enemy):
+        if character_rect.colliderect(enemy_rect[i]):
+            print("충돌..")
+            running = False
+
+    screen.blit(background, (0, 0)) # 배경 그리기
+    screen.blit(character, (character_x_pos, character_y_pos))  # 캐릭터 그리기
+    for i in range(len_enemy):
+        screen.blit(enemy[i], (enemy_x_pos[i], enemy_y_pos[i]))  # 캐릭터 그리기
+
+    # 타이머 집어 넣기
+    # 경과 시간 계산
+    elapsed_time =(pygame.time.get_ticks() - start_ticks) / 1000
+    # 경과시간(ms)을 1000으로 나누어서 초(s)단위로 표시
+
+    timer = game_font.render(str(int(total_time - elapsed_time)), True, (255,255,255))
+                                            # 출력할 글자, True, 글자 색상
+    screen.blit(timer, (10, 10))
+
+    # 만약 시간이 0 이하면 게임 종료
+    if total_time - elapsed_time <= 0:
+        print("타임아웃")
+        running = False
+
+    pygame.display.update() # 게임화면을 다시 그리기
+
+# 잠시 대기
+pygame.time.delay(2000) # 2초 대기
+# pygame 종료
+pygame.quit()
